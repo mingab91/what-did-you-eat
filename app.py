c55371158cb5decc847858049f300619ffcbc3a8
semyung oh <semyung.dev@gmail.com>
2021-09-13 17:22:41 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '잘부탁 드립니다!'
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
