861e05dd9d04a73a5f9898c36ff0873dc936f64a
semyung oh <semyung.dev@gmail.com>
2021-09-17 15:41:05 +0900

    Change markup and update validation for posting
    Still on progressing...

--- static/index.js
+++ static/index.js
@@ -1,4 +1,15 @@
-
+const validateExtension = (event) => {
+    const that = event.currentTarget;
+    const extension = that.value.match(/\.([^\.]+)$/)[1];
+    switch (extension) {
+        case 'jpg':
+        case 'png':
+            break;
+        default:
+            alert('올바르지 않은 파일 확장자입니다. png 또는 jpg 파일을 등록해주세요.');
+            that.value = '';
+    }
+};
 
 function find_food() {
     const food = $('#search-bar').val();
@@ -8,8 +19,14 @@ function find_food() {
 function post() {
     const title = $('#title-post').val();
     const file = $('#input-pic')[0].files[0];
-    const comment = $("#comment-post").val();
     const day = $("#day-post").val();
+    const comment = $("#comment-post").val();
+
+    if (!title || !file || !day || !comment) {
+        alert('게시물의 각 항목을 모두 입력해주세요');
+        return;
+    }
+
     const formData = new FormData();
     const now = Date.now().toString();
 
@@ -38,5 +55,5 @@ function post() {
 
 $('.logout-button').on('click', () => {
     $.removeCookie('mytoken', {path: '/'});
-    window.location.href = '/login'
+    window.location.href = '/login';
 });
